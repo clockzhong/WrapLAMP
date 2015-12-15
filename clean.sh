69fcd317a72c2e835370926646a7ee4874b52889
clock <clock@CleanUbuntu14.04Server64>
2015-12-15 18:13:49 +0800

    finished clean.sh

--- clean.sh
+++ clean.sh
@@ -6,6 +6,11 @@ sudo userdel mysql
 sudo groupdel mysql
 
 #remove MYSQL installed directory
-rm -fr ${MysqlInstallingPath}
+sudo rm -fr ${MysqlInstallingPath}
 
+#remove Php installed direcotry
+sudo rm -fr ${PhpInstallingPath}
+
+#remove Apache installed direcotry
+sudo rm -fr ${ApacheInstallingPath}
 
